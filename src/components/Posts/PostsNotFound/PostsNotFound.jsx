f7e390d3f3d9225c2e0461c87d5cba0ae8243ab6
imykhailychenko <igor.c.m@ukr.net>
2022-09-13 10:23:31 +0200

    module-3-lesson-2

--- src/components/Posts/PostsNotFound/PostsNotFound.jsx
+++ src/components/Posts/PostsNotFound/PostsNotFound.jsx
@@ -1,10 +1,10 @@
 import image from './713761_big_8857bc5285.jpeg';
 
-export const NotFound = () => {
+export const PostsNotFound = () => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
       <img src={image} alt="not found" style={{ width: 300 }} />
-      <p className="my-3">Произошол отрицательний поиск. Потерь нет!</p>
+      <p className="my-3">Произошел отрицательный поиск. Потерь нет!</p>
     </div>
   );
 };
