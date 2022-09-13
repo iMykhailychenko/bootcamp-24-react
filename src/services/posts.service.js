e1e07b110d846e5803223aacd31cd4c85faa78f3
imykhailychenko <igor.c.m@ukr.net>
2022-09-13 14:11:26 +0200

    module-3-lesson-2

--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -1,16 +1,13 @@
 import axios from 'axios';
 
 const postsApi = axios.create({
-  baseURL: 'http://70.34.201.18:8080',
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? 'https://taupe-croissant-c4162a.netlify.app/api'
+      : 'http://70.34.201.18:8080',
 });
 
 export const getPostsService = async params => {
   const { data } = await postsApi.get('/posts', { params });
   return data;
 };
-
-// {
-//     params: {
-//         page: 1
-//     }
-// }
