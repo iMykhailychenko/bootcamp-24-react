b7ca340b9c4eb337bc304780d62f9cdae72f3f82
imykhailychenko <igor.c.m@ukr.net>
2022-09-15 12:19:52 +0200

    module-4

---
+++ src/components/Timer/TimerComponent.jsx
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+
+import { Modal } from '../Modal';
+
+import { TimerModal } from './TimerModal';
+
+export class TimerComponent extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  toggle = () => {
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  };
+
+  render() {
+    const { isOpen } = this.state;
+    return (
+      <div className="d-flex mb-5">
+        <button type="button" className="btn btn-primary" onClick={this.toggle}>
+          Open timer
+        </button>
+
+        {isOpen && (
+          <Modal onModalClose={this.toggle}>
+            <TimerModal />
+          </Modal>
+        )}
+      </div>
+    );
+  }
+}
