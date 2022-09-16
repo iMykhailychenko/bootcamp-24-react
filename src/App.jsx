e2b6a8f002361f07280cbffe0bbc9212743f9621
imykhailychenko <igor.c.m@ukr.net>
2022-09-16 14:37:50 +0200

    module-4-lesson-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { ConfettiContainer } from './components/Confetti';
-import { Counter } from './components/Counter/Counter';
+import { Counter } from './components/Counter';
 import { Header, Layout } from './components/Layout';
 // import { Memo } from './components/Memo/Memo';s
 import { Posts } from './components/Posts';
-import { Rerender } from './components/Rerender/Rerender';
 import { AuthProvider } from './context/auth.context';
 
 export const App = () => {
