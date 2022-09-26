db15cd6a6f8c06a11657a9f81b69c5bd4e84a98d
imykhailychenko <igor.c.m@ukr.net>
2022-09-26 12:09:26 +0200

    module-7-redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
 import { getPostsService } from '../services/posts.service';
 
 import { initialState } from './initial-state';
