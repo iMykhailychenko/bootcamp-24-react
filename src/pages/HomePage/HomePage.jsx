4000ac5d2d0e240ebd0de4fec0ad15d74abe13af
imykhailychenko <igor.c.m@ukr.net>
2022-10-07 09:15:42 +0200

    done header

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,10 +4,7 @@ export const HomePage = () => {
       <div className="p-5 mb-4 bg-white border rounded-3">
         <div className="container-fluid py-5">
           <h1 className="display-5 fw-bold">Custom jumbotron</h1>
-          <p className="col-md-8 fs-4">
-            Using a series of utilities, you can create this jumbotron, just like the one in previous versions of
-            Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.
-          </p>
+          <p className="col-md-8 fs-4">Using a series of utilities</p>
           <button className="btn btn-primary btn-lg" type="button">
             Marketing info
           </button>
